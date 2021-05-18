c0708f3094772feeddd5090cda7d01c68640e01d
이승윤 <yyuunn17@naver.com>
2021-05-18 22:01:28 +0900

    [UPDATE] ErrorCode enum

--- src/main/java/com/sushiblog/backendv2/error/handler/ErrorCode.java
+++ src/main/java/com/sushiblog/backendv2/error/handler/ErrorCode.java
@@ -8,19 +8,18 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode { //대충 쓰면 나중에 오류날 때 어떤 오류인지 확인하기 어렵다.
-    FORMAT_INCORRECT(400,"String format is incorrect"),
+    FORMAT_INCORRECT(400,"String Format Incorrect"),
+    INVALID_TOKEN(400, "Invalid Token"), //유효하지 않은
 
-    INVALID_TOKEN(400, "Invalid Token"),
-    EXPIRED_TOKEN(401, "Token is Expired"),
-    UNAUTHENTICATED(401, "UnAuthenticated"),
-    NOT_ACCESSIBLE(401, "Check the token"),
+    NOT_ACCESSIBLE(401, "Check The Token"),
 
     POST_NOT_FOUND(404, "Post Not Found"),
     USER_NOT_FOUND(404, "User Not Found"),
     CATEGORY_NOT_FOUND(404, "Category Not Found"),
     IMAGE_NOT_FOUND(404, "Image Not Found"),
 
-    ALREADY_EXIST(409, "Already exists");
+    NICKNAME_ALREADY_EXIST(409, "Nickname Already Exists"),
+    EMAIL_ALREADY_EXIST(409, "Email Already Exists");
 
 
     private final int status;
