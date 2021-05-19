86cd1b2ad76a7bd790a48adc02c7fb35e3ede158
이승윤 <yyuunn17@naver.com>
2021-05-19 23:30:55 +0900

    [UPDATE] 사용하지 않는 매개변수 지움

--- src/main/java/com/sushiblog/backendv2/config/EmbeddedRedisConfig.java
+++ src/main/java/com/sushiblog/backendv2/config/EmbeddedRedisConfig.java
@@ -1,6 +1,5 @@
 package com.sushiblog.backendv2.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
@@ -14,7 +13,7 @@ public class EmbeddedRedisConfig {
 
     private final RedisServer redisServer;
 
-    public EmbeddedRedisConfig(@Value("${spring.redis.port}") int redisPort) {
+    public EmbeddedRedisConfig() {
         this.redisServer = RedisServer.builder()
                 .setting("maxheap 128M")
                 .build();
