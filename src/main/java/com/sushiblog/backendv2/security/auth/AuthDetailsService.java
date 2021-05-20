8b6ea5a7debb9edaa2e1e10865d2fc8006260886
이승윤 <yyuunn17@naver.com>
2021-05-20 17:24:37 +0900

    [UPDATE] 반환자료형 변경

--- src/main/java/com/sushiblog/backendv2/security/auth/AuthDetailsService.java
+++ src/main/java/com/sushiblog/backendv2/security/auth/AuthDetailsService.java
@@ -3,7 +3,6 @@ package com.sushiblog.backendv2.security.auth;
 import com.sushiblog.backendv2.entity.user.UserRepository;
 import com.sushiblog.backendv2.error.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -15,7 +14,7 @@ public class AuthDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException { //TokenProvider
+    public AuthDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException { //TokenProvider에서 사용
         //pk를 받아서 유저를 찾고 AuthDetails에 들어가서 정보 확인
         //이 메소드를 구현해야만 나중에 AuthenticationFacade에서 가져올 수 있다.
         return userRepository.findById(userEmail)
